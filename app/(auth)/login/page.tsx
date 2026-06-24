7e15f77d6c56756e70c53fc9b65b92c8cc951473
thoobi <sydneydaniel76@gmail.com>
2026-06-24 15:53:28 +0100

    fix: this handles the screen height issues for desktop

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,18 +1,18 @@
 import LoginForm from "@/components/auth/forms/login_form";
 const LoginPage = () => {
-  return (
-    <div className="flex flex-col h-full items-center justify-start w-full gap-8 max-lg:gap-5 bg-white">
-      <div className="flex flex-col gap-2 items-center">
-        <h2 className="text-foreground text-[32px]/[120%] lg:text-3xl/[120%] max-md:text-2xl/[120%] max-xs:text-lg tracking-tight font-medium">
-          Welcome back
-        </h2>
-        <p className="text-foreground text-base/[120%] max-sm:text-sm/[120%] font-normal text-center">
-          Enter your credentials to access your account
-        </p>
-      </div>
-      <LoginForm />
-    </div>
-  );
+	return (
+		<div className="flex flex-col h-full items-center justify-start w-full gap-8 max-lg:gap-5 bg-white">
+			<div className="flex flex-col gap-2 items-center">
+				<h2 className="text-foreground text-[32px]/[120%] lg:text-3xl/[120%] max-md:text-2xl/[120%] max-xs:text-lg tracking-tight font-medium">
+					Welcome back
+				</h2>
+				<p className="text-foreground text-base/[120%] max-sm:text-sm/[120%] font-normal text-center">
+					Enter your credentials to access your account
+				</p>
+			</div>
+			<LoginForm />
+		</div>
+	);
 };
 
 export default LoginPage;
--- components/auth/halfside.tsx
+++ components/auth/halfside.tsx
@@ -8,87 +8,87 @@ import { StaticImageData } from "next/image";
 import Image from "next/image";
 
 interface DataProp {
-  type: string;
-  title?: string;
-  description?: string;
-  url: StaticImageData;
+	type: string;
+	title?: string;
+	description?: string;
+	url: StaticImageData;
 }
 
 const halfSideData: DataProp[] = [
-  {
-    type: "login",
-    title: "Your relationships Streamlined. Your growth Accelerated.",
-    description:
-      "CRM360 helps you manage leads, close deals, and build lasting customer relationships - all in one place.",
-    url: Analytics,
-  },
-  {
-    type: "register",
-    title: "Grow relationships. Close more deals.All in one place.",
-    description:
-      "CRM360 helps you manage leads, close deals, and build lasting customer relationships - all in one place.",
-    url: Analytics,
-  },
-  {
-    type: "forgot-password",
-    url: ForgotImg,
-  },
-  {
-    type: "verification",
-    url: OtpImg,
-  },
-  {
-    type: "otp-verification",
-    url: OtpImg,
-  },
-  {
-    type: "reset-password",
-    url: ResetImg,
-  },
-  {
-    type: "accept-invite",
-    url: ResetImg,
-  },
+	{
+		type: "login",
+		title: "Your relationships Streamlined. Your growth Accelerated.",
+		description:
+			"CRM360 helps you manage leads, close deals, and build lasting customer relationships - all in one place.",
+		url: Analytics,
+	},
+	{
+		type: "register",
+		title: "Grow relationships. Close more deals.All in one place.",
+		description:
+			"CRM360 helps you manage leads, close deals, and build lasting customer relationships - all in one place.",
+		url: Analytics,
+	},
+	{
+		type: "forgot-password",
+		url: ForgotImg,
+	},
+	{
+		type: "verification",
+		url: OtpImg,
+	},
+	{
+		type: "otp-verification",
+		url: OtpImg,
+	},
+	{
+		type: "reset-password",
+		url: ResetImg,
+	},
+	{
+		type: "accept-invite",
+		url: ResetImg,
+	},
 ];
 
 export default function HalfSide() {
-  const pathname = usePathname();
-  const value = Object(
-    halfSideData.find((data) => pathname.includes(data.type)),
-  );
+	const pathname = usePathname();
+	const value = Object(
+		halfSideData.find((data) => pathname.includes(data.type)),
+	);
 
-  const otherHalf = [
-    "verification",
-    "forgot-password",
-    "reset-password",
-    "otp-verification",
-  ];
+	const otherHalf = [
+		"verification",
+		"forgot-password",
+		"reset-password",
+		"otp-verification",
+	];
 
-  return (
-    <div
-      className={`lg:w-1/2 max-lg:hidden h-full bg-[#FFF6EC] flex flex-col justify-center xl:py-8 py-3 px-5 xl:px-12 rounded-[16px] gap-15 lg:gap-10 ${(value.type !== "login" || value.type === "register") && "min-h-[95svh] "}`}
-    >
-      <div
-        className={`flex items-center justify-center w-full min-h-full ${otherHalf.includes(value.type) && "bg-white"} rounded-[16px]`}
-      >
-        <Image
-          src={value.url}
-          alt="analytics"
-          width={800}
-          height={800}
-          className="max-h-full xl:w-[85%] w-auto object-cover"
-        />
-      </div>
-      {(value.type === "login" || value.type === "register") && (
-        <div className="flex flex-col gap-4 items-center justify-center w-full">
-          <h1 className="text-[38px]/[120%] text-foreground tracking-tight font-medium text-center">
-            {value.title}
-          </h1>
-          <p className="text-base text-center font-normal text-foreground">
-            {value.description}
-          </p>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div
+			className={`lg:w-1/2 max-lg:hidden h-full bg-[#FFF6EC] flex flex-col justify-center xl:py-8 py-4  px-5 xl:px-12 rounded-[16px] gap-15 lg:gap-10 ${value.type === "login" || value.type === "register" ? "min-h-[95svh] " : "min-h-[95svh]"} `}
+		>
+			<div
+				className={`flex items-center justify-center w-full min-h-full ${otherHalf.includes(value.type) && "bg-white"} rounded-[16px]`}
+			>
+				<Image
+					src={value.url}
+					alt="analytics"
+					width={800}
+					height={800}
+					className="max-h-full xl:w-[85%] w-auto object-cover"
+				/>
+			</div>
+			{(value.type === "login" || value.type === "register") && (
+				<div className="flex flex-col gap-4 items-center justify-center w-full">
+					<h1 className="text-[38px]/[120%] text-foreground tracking-tight font-medium text-center">
+						{value.title}
+					</h1>
+					<p className="text-base text-center font-normal text-foreground">
+						{value.description}
+					</p>
+				</div>
+			)}
+		</div>
+	);
 }
